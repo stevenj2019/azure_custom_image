d926520a81395a3bb3b3e2fd2a46c5d168589ac5
Steven Johnson <steven.j2019@pm.me>
2020-07-21 15:45:25 +0100

    changes to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-import socker 
+import socket 
 
 app = Flask(__name__)
 
@@ -8,3 +8,6 @@ host = socket.gethostname()
 @app.route('/')
 def index():
 	return "<h1>Hello World!, I'm deployed automatically on " + str(host) + " :) </h1>"
+
+if __name__ == '__main__':
+	app.run(host=0.0.0.0, port=5000, debug=True)
