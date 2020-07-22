34a5625a44bedd65a300fbee6634de98614bb39e
Steven Johnson <steven.j2019@pm.me>
2020-07-22 15:59:45 +0100

    fix

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def index():
 	return "<h1>Hello World!, I'm deployed automatically on " + str(host) + " :) </h1>"
 
 if __name__ == '__main__':
-	app.run(host=0.0.0.0, port=5000, debug=True)
+	app.run(host='0.0.0.0', port=5000, debug=True)
--- script.sh
+++ script.sh
@@ -5,3 +5,5 @@ az group create -n flask-app -l uksouth
 az vm availability-set create -g flask-app -n flask-set -l uksouth --platform-fault-domain-count 2 --platform-update-domain-count 5
 
 az vm create -g flask-app -n flask-server -l uksouth --image UbuntuLTS --availability-set flask-set --generate-ssh-keys --custom-data cloud-init.txt
+
+az vm open-port --resource group flask-app --name flask-server --port 5000
